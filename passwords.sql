b9103ba68ae5552a1e018aa724fbce4e65d62ef9
Frank Angelone <frank@frankangelone.com>
2014-03-30 17:12:54 -0400

    Add SQL file for database connectivity

---
+++ passwords.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 30, 2014 at 05:04 PM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `passwords`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passcode`
+--
+
+CREATE TABLE `passcode` (
+  `passcode` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `passcode`
+--
+
+INSERT INTO `passcode` VALUES('dc724af18fbdd4e59189f5fe768a5f8311527050');
